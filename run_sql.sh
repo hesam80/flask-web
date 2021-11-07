5213ca4d592f1dab918dd0a48258981cc56de6eb
hesam80 <hm.hoseyni@gamil.com>
2021-11-07 10:44:18 +0000

    last commit to deploy on fandogh

--- run_sql.sh
+++ run_sql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python main.py
 python -m pip install --upgrade pip
-python fandogh login --username hesam80 --password sa13801380
+fandogh login --username hesam80 --password sa13801380
+fandogh apply -f  flaskapp.yml
