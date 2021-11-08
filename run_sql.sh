6a4245f0c68a66d2f067b94559553669e20a096c
hesam80 <hm.hoseyni@gmail.com>
2021-11-08 20:03:07 +0000

    run_sql.sh check

--- run_sql.sh
+++ run_sql.sh
@@ -4,3 +4,4 @@ python -m pip install --upgrade pip
 python -m pip install fandogh-cli
 fandogh login --username hesam80 --password sa13801380
 fandogh service apply  -f flaskapp.yml
+echo "fandogh log"
