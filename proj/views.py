19adc2a38d356b44c6bb47c4aadf5c0b6835e7c4
hesam80 <hm.hoseyni@gmail.com>
2021-11-13 19:56:43 +0000

    dev Flask-Web-App-Tutorial

--- proj/views.py
+++ proj/views.py
@@ -1,10 +1,37 @@
-from flask import Blueprint, Flask, flash, redirect, render_template, request, url_for
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
-@views.route('/')
-def view():
-    """Return a friendly HTTP greeting."""
-    message = "simpleapps redeployedd Wow how it's Nice!20000"
-    congramessage="Congratulations, you successfully deployed a container image to Cloud Run!"
-    """Get Cloud Run environment variables."""
-    return render_template('index.html' ,congmsg=congramessage,message=message)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
