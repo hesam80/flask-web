d9e513f5bd1b86ec8b357a8e7304909f6012bda1
hesam80 <hm.hoseyni@gmail.com>
2021-11-23 12:37:40 +0330

    tst lint

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ API_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=m
 
 def query_api(city):
     try:
-        #print(API_URL.format(city, API_KEY))
+        #tst lint print(API_URL.format(city, API_KEY))
         data = requests.get(API_URL.format(city, API_KEY)).json()
     except Exception as exc:
         print(exc)
