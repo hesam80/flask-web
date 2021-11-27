95c875dd63fa3a504f20d77be283955f421fb0d3
hesam80 <hm.hoseyni@gmail.com>
2021-11-27 20:28:40 +0000

    if elseif else0.4

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const price = 99;
 const newprice= 189;
 if(newprice>price){
-    console.log('price is'.newprice);
+    console.log(newprice)
 }
 else if(newprice<price){
-    console.log('price is ${price}');
+    console.log(price)
 }
 else{ console.log('same value price and newprice')}
 
