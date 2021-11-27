e4cc94e07fc00c4fde718a23eb97088b1bdb6e78
hesam80 <hm.hoseyni@gmail.com>
2021-11-27 19:13:32 +0000

    js const array

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 //variables
-
 let number = 20 ;
+//Array and objects
+const products=[
+{
+id:1,
+title: 'Book 1',
+price: 99,
+exist: true,
+},
+{
+    id:2,
+    title: 'Book 2',
+    price: 89,
+    exist: false,
+    },
+    {
+        id:3,
+        title: 'Book 3',
+        price: 79,
+        exist: true,
+        },
+
+
+]
+//destructering
+const product=['film', 99, true];
+const [title, price]= product
+
+
+
 console.log("hello");
 console.log(number);
+console.log(product);
+console.log(title);
+
