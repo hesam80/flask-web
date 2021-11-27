d9d63dae6810b7ea7dd18e95f7709a521c0a1543
hesam80 <hm.hoseyni@gmail.com>
2021-11-27 20:18:51 +0000

    if elseif else

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-//JSON JSONIFY JSON.stringify(products)  
-//Objects and destructuring
-const oneproductobj={title:'film',price:99,exist:true}
+//if condition
+const price = 99;
+const newprice= 189;
+if(newprice>price){
+    console.log('price is ${newprice}');
 
-//arraye az objects ha!!
-const products=[
-{
-id:1,
-title: 'Book 1',
-price: 99,
-exist: true,
-},
-{
-    id:2,
-    title: 'Book 2',
-    price: 89,
-    exist: false,
-    },
-    {
-        id:3,
-        title: 'Book 3',
-        price: 79,
-        exist: true,
-        },
-
-
-]
-//vaghti object ro mikhahim befrstim be platform python
-const data= JSON.stringify(products);
-
-//vaghti reshte string miad va tabdilesh konim be object
-const responce= JSON.parse(data)
-//for
-for(let item=0 ; item< products.length; item++){
-    console.log(products[item].title)
+elseif(newprice<price)
+    console.log('price is ${price}');
 }
+else{ console.log('same value price and newprice')}
 
