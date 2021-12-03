9b27eac5fbcb0efc58fb01fe0c566ba34188f69c
hesam80 <hm.hoseyni@gmail.com>
2021-12-03 14:24:12 +0000

    api backend frontend connected

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -14,8 +14,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>hello</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload."3"
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>The current time is {currentTime}.</p>
         <a
