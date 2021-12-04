f19bf9d9653199cbfe512ce432d2067a00cbbc50
hesam80 <hm.hoseyni@gmail.com>
2021-12-04 23:23:54 +0330

    useDarkMode.js create

---
+++ myapp/src/useDarkMode.js
@@ -0,0 +1,21 @@
+import {useState, useEffect} from 'react'
+
+const useDarkMode = ()=> {
+	const [theme,setTheme]=useState('light')
+
+	const toggleTheme= ()=>{
+		if (theme==='light') {
+			window.localStorage.setItem('theme','dark')
+			setTheme('dark')
+		}
+		else{
+			window.localStorage.setItem('theme', 'light')
+			setTheme('light')
+		}
+	}
+
+	useEffect(()=> {
+		const localTheme = window.localStorage.getItem('theme')
+	}, [])
+}
+export default useDarkMode
