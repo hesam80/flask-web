f8e84cdc2b0a48ea220ef7d10733278ff50cab03
hesam80 <hm.hoseyni@gmail.com>
2021-12-07 20:44:09 +0000

    return {'weather':weather}

--- myapp/api/api.py
+++ myapp/api/api.py
@@ -12,7 +12,7 @@ def get_current_weather(city='tehran'):
     API_KEY = '92ef7fe4196f1fe341c896bb105291b3'
     API_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}'
     weather = requests.get(API_URL.format(city, API_KEY)).json()
-    return {'wether': weather}
+    return {'weather':weather}
 
 
 #Running and Controlling the script
