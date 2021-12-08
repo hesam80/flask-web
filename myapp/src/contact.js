292d1bc2f9c320a33e3b78a823c5f3a032819aa9
hesam80 <hm.hoseyni@gmail.com>
2021-12-08 13:49:40 +0330

    add component base classs

---
+++ myapp/src/contact.js
@@ -0,0 +1,6 @@
+import React from 'react'
+class Contact extends React.Component{
+	render(){
+		return <h1>Contact</h1>
+	}
+}
---
+++ myapp/src/users.js
@@ -0,0 +1,6 @@
+import React from 'react'
+class Users extnds React.Component{
+	render(){
+	return <h1>Users</h1>
+	}
+}
