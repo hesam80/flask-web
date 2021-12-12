823ef8127bf4ca82ba21f8cd0ec3ca9f24ed8ab5
hesam80 <hm.hoseyni@gamil.com>
2021-12-12 12:07:43 +0000

    install node-man

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Oh Man YAEH!!!!</h1>
+        <h1>Oh Man YES!!!!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
