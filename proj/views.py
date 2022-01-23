2b8958b0d80aa9c5eee75a99e63250d0944dfb4b
hesam80 <hm.hoseyni@gamil.com>
2022-01-23 09:34:08 +0000

    update 23-jan

--- proj/views.py
+++ proj/views.py
@@ -5,6 +5,6 @@ views = Blueprint('views', __name__)
 def view():
     """Return a friendly HTTP greeting."""
     message = "simpleapps redeployedd Wow how it's Nice!!"
-    congramessage="Congratulations, you successfully deployed a container image to Cloud Run at 2022-jan-22!"
+    congramessage="Congratulations, you successfully deployed a container image to Cloud Run at 2022-jan-23!"
     """Get Cloud Run environment variables."""
     return render_template('index.html' ,congmsg=congramessage,message=message)
