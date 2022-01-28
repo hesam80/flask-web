0935554430fb6b597c5627f050f51f2cec764fbc
hesam80 <hm.hoseyni@gmail.com>
2022-01-28 16:03:02 +0330

    tst deploy

--- run_sql.sh
+++ run_sql.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 python main.py
-python -m pip install --upgrade pip
-python -m pip install fandogh-cli
-fandogh login --username hesam80 --password sa13801380
-fandogh service deploy --image hesam80/flaskapp --version latest
 echo "fandogh log"
