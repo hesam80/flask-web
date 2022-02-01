0e331673e1d15e03721b2bb68921a4c5f374d3ad
hesam80 <hm.hoseyni@gmail.com>
2022-02-01 20:29:11 +0000

    , today

--- proj/views.py
+++ proj/views.py
@@ -4,9 +4,9 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def view():
 
-    today = date.today()
+    today = str(date.today()) 
     """Return a friendly HTTP greeting."""
     message = "simpleapps redeployedd Wow how it's Nice!!"
-    congramessage="Congratulations, you successfully deployed  container image to Cloud Run at:".today
+    congramessage="Congratulations, you successfully deployed  container image to Cloud Run at:", today
     """Get Cloud Run environment variables."""
     return render_template('index.html' ,congmsg=congramessage,message=message)
