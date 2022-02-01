459ba9cff08e3c43e8a6cab5bfb2c5b340069998
hesam80 <hm.hoseyni@gmail.com>
2022-02-01 23:53:46 +0330

    today = date.today()

--- proj/views.py
+++ proj/views.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, Flask, flash, redirect, render_template, request, url_for
-
+from datetime import date
 views = Blueprint('views', __name__)
 @views.route('/')
 def view():
+
+    today = date.today()
     """Return a friendly HTTP greeting."""
     message = "simpleapps redeployedd Wow how it's Nice!!"
-    congramessage="Congratulations, you successfully deployed  container image to Cloud Run at 2022-Feb-01!!"
+    congramessage="Congratulations, you successfully deployed  container image to Cloud Run at:".today
     """Get Cloud Run environment variables."""
     return render_template('index.html' ,congmsg=congramessage,message=message)
