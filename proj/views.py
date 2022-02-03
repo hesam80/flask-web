f9a02920c881edf8a3eb8060e1eb580ee5de2ee8
hesam80 <hm.hoseyni@gmail.com>
2022-02-03 20:27:24 +0330

    workflow safed

--- proj/views.py
+++ proj/views.py
@@ -5,12 +5,7 @@ views = Blueprint('views', __name__)
 def view():
 
     today = str(date.today()) 
-    """Return a friendly HTTP greeting."""
     message = "simpleapps redeployedd Wow how it's Nice!!"
-<<<<<<< HEAD
-    congramessage="Congratulations, you successfully deployed  container image to Cloud Run at 2022-Feb-02"
-=======
     congramessage="Congratulations, you successfully deployed  container image to Cloud Run at:"
->>>>>>> circleci
-    """Get Cloud Run environment variables."""
+
     return render_template('index.html' ,congmsg=congramessage,message=message ,today=today)
--- run_sql.sh
+++ run_sql.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python main.py
-python -m pip install --upgrade pip
-python -m pip install fandogh-cli
-fandogh login --username hesam80 --password sa13801380
+python pip install --upgrade pip
+python pip install fandogh-cli
 fandogh service deploy --image hesam80/flaskapp --version latest
 echo "fandogh log"
