979658d01f33b92aba78202e4573ab6433b1bacb
hesam80 <hm.hoseyni@gmail.com>
2022-02-03 19:17:08 +0000

    prac" "

--- run_sql.sh
+++ run_sql.sh
@@ -2,5 +2,7 @@
 python main.py
 python pip install --upgrade pip
 python pip install fandogh-cli
+git checkout --force circleci
+git --no-pager log --no-color -n 1 
 fandogh service deploy --image hesam80/flaskapp --version latest
 echo "fandogh log"
