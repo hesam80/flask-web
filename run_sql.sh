630593178c8c1cfb11e0e841e3d02510e66a92d7
hesam80 <hm.hoseyni@gmail.com>
2022-02-05 10:39:53 +0330

    git --no-pager log --no-color -n 1

--- run_sql.sh
+++ run_sql.sh
@@ -2,6 +2,6 @@
 python main.py
 python pip install --upgrade pip
 python pip install fandogh-cli
-coomit = git --no-pager log --no-color -n 1 --format='%h'
+commit = git --no-pager log --no-color -n 1 --format='%h'
 fandogh service deploy --image hesam80/flaskapp --version $commit
 echo "fandogh log"
