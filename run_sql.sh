30e6a756b3c93f82f985b0b81a082f7e0497933d
hesam80 <hm.hoseyni@gmail.com>
2022-12-16 13:10:00 +0000

    16 dec

--- run_sql.sh
+++ run_sql.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 python main.py
-/usr/bin/python pip install --upgrade pip
-/usr/bin/python pip install fandogh-cli --upgrade
 git checkout --force circleci
 git --no-pager log --no-color -n 1 --format='%h'
-fandogh service deploy --image hesam80/flaskapp --version latest
-echo "fandogh login"
+
