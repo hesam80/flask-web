6a7b96f48d57a5826a16d28ccbf58b8254904561
hesam80 <hm.hoseyni@gmail.com>
2024-10-20 13:58:06 +0330

    remove findlocation function in view.py

--- proj/views.py
+++ proj/views.py
@@ -4,7 +4,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def view():
     today = str(date.today()) 
-    my_location=find_location()
+    
    
 
     
@@ -12,19 +12,3 @@ def view():
     congramessage="Congratulations, you successfully deployed  container image to Cloud Run at: "
 
     return render_template('index.html' ,congmsg=congramessage,message=message ,today=today,city=my_location[2])
-def find_location():
-    try:
-        response = requests.get('https://ipinfo.io')
-        data = response.json()
-        loc = data['loc'].split(',')
-        lat, long = float(loc[0]), float(loc[1])
-        city = data.get('city', 'Unknown')
-        state = data.get('region', 'Unknown')
-        return lat, long, city, state
-        #return lat, long
-    except:
-        #Displaying ther error message
-        print("Internet Not avialable")
-        #closing the program 
-        exit()
-        return False
