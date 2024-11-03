bbca32776807259190b51d3d36a8cadd0eacb354
hesam80 <hm.hoseyni@gmail.com>
2024-11-03 04:52:53 +0000

    add sleep 20 after docker run

--- run_sql.sh
+++ run_sql.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker run -d --name qualitycontrol -e LOGIN="mahanairlines.com@gmail.com" --device=/dev/net/tun --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --log-opt max-size=10m --log-opt max-file=3 --restart=always ghcr.io/org004/qc:latest
+sleep 20
+docker ps
