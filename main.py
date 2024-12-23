3012bae26d9f9d33a9fa48622bcd99dfa73a5ef1
hesam80 <hm.hoseyni@gmail.com>
2024-12-23 14:17:05 +0330

    asyncio

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
-
+import time , os
+import asyncio
+import time
 def __init__():
     print("hello")
+    path = os.path()
 
-if __name__ == '__main__':
-    __init__()
+
+
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+asyncio.run(main())
+async def main2():
+    task1 = asyncio.create_task(
+        say_after(1, 'hello'))
+
+    task2 = asyncio.create_task(
+        say_after(2, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+asyncio.run(main2())
+
+def main_advanced():
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(
+            say_after(1, 'hello'))
+
+        task2 = tg.create_task(
+            say_after(2, 'world'))
+
+        print(f"started at {time.strftime('%X')}")
+
+    # The await is implicit when the context manager exits.
+
+         print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(main_advanced())
