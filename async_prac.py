5389385c6fe070ee91c3bdfff0c6ab110006cf8f
hesam80 <hm.hoseyni@gmail.com>
2024-12-30 06:17:20 +0000

    counter

--- async_prac.py
+++ async_prac.py
@@ -1,42 +1,20 @@
 import asyncio
-import contextvars
+#!/usr/bin/env python3
+# countasync.py
 
-client_addr_var = contextvars.ContextVar('client_addr')
 
-def render_goodbye():
-    # The address of the currently handled client can be accessed
-    # without passing it explicitly to this function.
 
-    client_addr = client_addr_var.get()
-    return f'Good bye, client @ {client_addr}\r\n'.encode()
-
-async def handle_request(reader, writer):
-    addr = writer.transport.get_extra_info('socket').getpeername()
-    client_addr_var.set(addr)
-
-    # In any code that we call is now possible to get
-    # client's address by calling 'client_addr_var.get()'.
-
-    while True:
-        line = await reader.readline()
-        print(line)
-        if not line.strip():
-            break
-
-    writer.write(b'HTTP/1.1 200 OK\r\n')  # status line
-    writer.write(b'\r\n')  # headers
-    writer.write(render_goodbye())  # body
-    writer.close()
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
 
 async def main():
-    srv = await asyncio.start_server(
-        handle_request, '127.0.0.1', 8081)
+    await asyncio.gather(count(), count(), count())
 
-    async with srv:
-        await srv.serve_forever()
-
-asyncio.run(main())
-
-# To test it you can use telnet or curl:
-#     telnet 127.0.0.1 8081
-#     curl 127.0.0.1:8081
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
