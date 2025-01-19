5e22078e265da0036e3b8bec3cc53489dd7d8ff7
hesam80 <hm.hoseyni@gmail.com>
2025-01-19 06:38:43 +0000

    everything on asyncio update

--- async_prac.py
+++ async_prac.py
@@ -1,20 +1,43 @@
 import asyncio
+import time
+from main import say_after
 #!/usr/bin/env python3
 # countasync.py
 
+async def main_advanced():
+    #start_exec= time.perf_counter()
+    #from asyncio import TaskGroup
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(
+            count())
 
+        task2 = tg.create_task(
+            say_after(2, 'world'))
+
+        print(f"started at {time.strftime('%X')}")
+
+    # The await is implicit when the context manager exits.
+
+        print(f"finished at {time.strftime('%X')}")
+        
+
+#with asyncio.Runner() as runner:
+    #runner.run(main_advanced())
 
 async def count():
     print("One")
-    await asyncio.sleep(1)
+    #await asyncio.sleep(1)
+    await sleep(1)
     print("Two")
 
 async def main():
-    await asyncio.gather(count(), count(), count())
+    start_exec= time.perf_counter()
+    await asyncio.gather(main_advanced(), count(),say_after(1,'home'))
+    end_exec=time.perf_counter()
+    elapsed = end_exec-start_exec
+    print(f"{__file__} executed in {elapsed:.6f} seconds.")
 
-if __name__ == "__main__":
-    import time
-    s = time.perf_counter()
-    asyncio.run(main())
-    elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+
+    
+asyncio.run(main())
+    
--- main.py
+++ main.py
@@ -5,10 +5,6 @@ def __init__():
     print("hello")
     path = os.path()
 
-
-
-
-
 async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
@@ -44,22 +40,3 @@ async def main2():
     print(f"this task executed in {elapsed:.6f} seconds.")
     print(f"this task executed in {elapsed_miliseconds:.1f} miliseconds.")
 asyncio.run(main2())
-
-async def main_advanced():
-    #from asyncio import TaskGroup
-    async with asyncio.TaskGroup() as tg:
-        task1 = tg.create_task(
-            say_after(1, 'hello'))
-
-        task2 = tg.create_task(
-            say_after(2, 'world'))
-
-        print(f"started at {time.strftime('%X')}")
-
-    # The await is implicit when the context manager exits.
-
-        print(f"finished at {time.strftime('%X')}")
-        print(f"{__file__}execute")
-
-#with asyncio.Runner() as runner:
-    #runner.run(main_advanced())
